db25c6d48ebb51b5c9458299f53e5387029074fe
Nasseima <lindsaynasseima@gmail.com>
2024-06-26 12:18:32 -0400

    test

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Task One
+public class Main {
+    public static void main(String[] args) {
+// Declare two Integer variable.
+    int a, b;
+// Assign integer to each.
+     a = 10;
+     b = 20;
+// Assign the sum to a variable.
+    int intSum = a + b;
+// Print out the results.
+    System.out.println("The sum of these two variables is :" + intSum);
+
+
+// Task Two
+    // Declares two double variable
+    double c, d;
+    c = 3.14567;
+    d = 1000;
+
+// Assign the sum to a variable.
+        double doubleSum = c + d;
+// Print the results
+    System.out.println("The sum of these double variables is: " + doubleSum);
+
+
+// Task Three
+// Add an Integer and Double variables
+
+
+
+    }
+
+}
+
